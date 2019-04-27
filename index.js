59fd4368a4d634834d2e48542c105d3124a6245b
mac_mojave_2019 <priyankad96@gmail.com>
2019-04-26 22:19:53 -0700

    Component,Luzy Screen,Navigation,Redux,ImageSlider

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-//import App from './app/screens/home';
-//import App from './src/app/Login';
-//import App from './src/config';
-//import App from './src/navigation/appNavigator1';
-//import App from './src/navigation/demoAppNavigator';
+//import App from './app/screens/home';                 //image slider xcasses
+//import App from './src/app/Login';                    //Luzy Login Screen
+//import App from './src/config';                       //redux demo
+//import App from './src/navigation/appNavigator1';     //navigation demo
+//import App from './src/navigation/demoAppNavigator';  //demo  of component
 
 
 
